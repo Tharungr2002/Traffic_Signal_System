8e4dfb1f57f7f18497e13b9b4913e651d86bebf8
Tharun gr <tharungr2002@gmail.com>
2026-06-02 22:17:57 +0530

    Finish---

---
+++ Model/VehicleCounter.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class VehicleCounter {
+    private Direction direction;
+    private int count;
+    private long lastUpdate;
+}
+
+
+
+// used for automatic signal timing
